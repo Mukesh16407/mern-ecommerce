c36df39204fd64a6ea9e8a7c406d96aaba2d3d64
Mukesh16407 <mukesh.16407@gmail.com>
2023-02-20 09:49:34 +0530

    add media quary in header

--- client/src/Components/Nav/Header.jsx
+++ client/src/Components/Nav/Header.jsx
@@ -1,70 +1,79 @@
-import React from 'react'
-import { Menu } from 'antd';
-import { HomeOutlined ,UserOutlined,UserAddOutlined,  SettingOutlined ,UserDeleteOutlined } from '@ant-design/icons';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Menu } from "antd";
+import {FaShoppingCart} from 'react-icons/fa'
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { auth } from "../../firebase";
+import { signOut, updateEmail } from "firebase/auth";
+import { toast } from "react-toastify";
+import "./header.css";
 
-const items = [
-  {
-    label: <Link to="/">Home</Link>,
-    key: 'home',
-    icon: <HomeOutlined />,
-  },
-  {
-    label: 'User Name',
-    key: 'user',
-  
-    icon: <SettingOutlined />,
-    style: { marginRight:"63%" } ,
-    children: [
-      {
-        label: 'Option 1',
-        key: 'setting:1',
-       
-        
-      },
-      {
-        label: 'Option 2',
-        key: 'setting:2',
-       
-       
-      },
-    ],
-  },
-  {
-    label: <Link to="/register">Register</Link>,
-    key: 'register',
-    icon: <UserAddOutlined />,
-   
-  },
-  
-  {
-    label: <Link to="/login">Login</Link>,
-    key: 'login',
-    icon: <UserOutlined />,
-   
-   
-  },
-  {
-    label: <Link to="/login">Logout</Link>,
-    key: 'logout',
-    icon: <UserDeleteOutlined />,
-   
-   
-  },
-  
-];
-
-
+const logo = (
+  <div className="logo">
+    <NavLink to="/">
+      <h2>
+        E <span>Shop</span>.
+      </h2>
+    </NavLink>
+  </div>
+);
+const cart = (
+  <div className="cart">
+    <NavLink to="/cart">
+      Cart <FaShoppingCart size={20}/>
+      <p>0</p>
+    </NavLink>
+  </div>
+);
 export const Header = () => {
-
-  const [current, setCurrent] = useState('home');
-
+  const [current, setCurrent] = useState("home");
+  const [currentEmail, setCurrentEmail] = useState("");
   const handleClick = (e) => {
-    console.log('click ', e.key);
+    console.log("click ", e.key);
     setCurrent(e.key);
   };
+
+  // const logOutUser=()=>{
+  //   signOut(auth).then(() => {
+  //    toast.success("Sign Out Successfully")
+  //   }).catch((error) => {
+  //     toast.error(error.message)
+  //   });
+  // }
+  // monetring currently signin user
+  useEffect(() => {
+    // updateEmail(auth.currentUser, "user@example.com").then(() => {
+    //   console.log(auth.currentUser);
+    //   setCurrentEmail(auth.currentUser)
+    // }).catch((error) => {
+    //  setCurrentEmail("")
+    // });
+  }, []);
   return (
-  <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" items={items} />
-  )
-}
+    <>
+      <header>
+        <div className="header">{logo}</div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact Us</NavLink>
+            </li>
+          </ul>
+          <div className="header-right">
+            <span className="links">
+              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/register">Register</NavLink>
+              <NavLink to="/order-history">My Order</NavLink>
+            </span>
+            <span className="cart">
+              <NavLink to="/cart">{cart}</NavLink>
+            </span>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
--- client/src/Components/card/Card.jsx
+++ client/src/Components/card/Card.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './card.css'
-export const Card = ({children}) => {
+export const Card = ({children,cardClass}) => {
   return (
-    <div className='card'>
+    <div className={`${cardClass}`}>
         {children}
     </div>
   )
