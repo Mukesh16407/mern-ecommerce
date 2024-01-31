35b761043abcc4b5110e49e326f3aa7502460d45
Mukesh16407 <mukesh.16407@gmail.com>
2024-02-01 03:11:03 +0530

    stripe frontend work

--- client/src/pages/Payment/Payment.js
+++ client/src/pages/Payment/Payment.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+import "../../stripe.css";
+import { StripeCheckout } from "../../Components/StripeCheckout.js/StripeCheckout";
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 const Payment = () => {
   return (
     <div className="container p-5 text-center">
-      <div>Complete your purchase</div>
+      <h4>Complete your purchase</h4>
+      <Elements stripe={promise}>
+        <div className="col-md-8 offset-md-2">
+          <StripeCheckout />
+        </div>
+      </Elements>
     </div>
   );
 };
