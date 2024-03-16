b70d61e786dbe547cc7465bc384e2d5d71584aff
Mukesh16407 <mukesh.16407@gmail.com>
2024-03-16 15:41:38 +0530

    add COD

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -16,6 +16,7 @@ const {
   addToWishlist,
   wishlist,
   removeFromWishlist,
+  createCashOrder,
 } = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart); // save cart
@@ -24,6 +25,7 @@ router.delete("/user/cart", authCheck, emptyCart); // empty cart
 router.post("/user/address", authCheck, saveAddress);
 
 router.post("/user/order", authCheck, createOrder);
+router.post("/user/cash-order", authCheck, createCashOrder);
 router.get("/user/orders", authCheck, orders);
 // coupon
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
